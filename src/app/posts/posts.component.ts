c677f7f88ab6fccb0b5c1f12811847ab449a7067
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-01-31 10:10:46 -0800

    button colors altered

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -30,7 +30,7 @@ export class PostsComponent implements OnInit {
       .subscribe(res => {
         post['id'] = res.json().id; 
         this.posts.splice(0, 0, post)
-      })
+      }); 
   }
 
   deletePost(post) {
@@ -39,7 +39,17 @@ export class PostsComponent implements OnInit {
       .subscribe(res => {
         let index = this.posts.indexOf(post)
         this.posts.splice(index, 1); 
-      })
+      }); 
   }
 
+  updatePost(post) {
+    this.service.updatePost(post)
+      .subscribe(res => {
+        let index = this.posts.indexOf(post)
+        post['checked'] = res.json().checked; 
+        console.log(res.json(), "HERE IS THE RES")
+        console.log(post, "HERE IS THE POST")
+        this.posts.splice(index, 1, post)
+      }); 
+  }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -19,4 +19,9 @@ export class PostsService {
   deletePost(post) {
     return this.http.delete(this.url + '/' + post.id )
   }
+
+  updatePost(post) {
+    return this.http.put(this.url + '/' + post.id, post)
+  }
+  
 }
